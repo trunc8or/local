c3bb9c5813d7b1ce48f8049b5e75b11c2f604acd
trunc8or <roryobrien2905@gmail.com>
2026-06-22 21:34:50 +1000

    Update local_launch.py
    change for amcl

--- launch/local_launch.py
+++ launch/local_launch.py
@@ -1,24 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from ament_index_python.packages import get_package_share_directory
-import os
-
 def generate_launch_description():
 
 ```
-config_file = os.path.join(
-    get_package_share_directory('local'),
-    'config',
-    'local.yaml'
-)
-
 localization_node = Node(
     package='local',
     executable='localization_node',
     name='localization_node',
-    output='screen',
-    parameters=[config_file]
+    output='screen'
 )
 
 return LaunchDescription([
